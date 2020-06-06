cfdd77b349fbeab96d3e5b246ba38c30077ad35e
Poorna Kumar <poorna.kumar@gmail.com>
2020-06-06 01:50:55 -0700

    fix error in validation of CombineLabels

--- datasets/combine_labels.py
+++ datasets/combine_labels.py
@@ -26,17 +26,23 @@ class CombineLabels:
         current_idx = 0
         for s in self.category_groups:
             if isinstance(s, set):
-                for verb in s:
-                    assert verb not in self.to_remove
-                    self.action_to_idx[verb] = current_idx
+                for action in s:
+                    self._validate_not_to_remove_action(action)
+                    self.action_to_idx[action] = current_idx
                 self.final_category_names.append(" + ".join(sorted(s)))
             elif isinstance(s, str):
-                assert s not in self.to_remove
+                self._validate_not_to_remove_action(s)
                 self.action_to_idx[s] = current_idx
                 self.final_category_names.append(s)
             else:
                 raise TypeError(f"{s} is of unsupported type {type(s)}. Must be string or set.")
             current_idx += 1
+
+    def _validate_not_to_remove_action(self, action):
+        if action in self.to_remove:
+            raise ValueError(
+                f"{action} in set to be removed {self.to_remove}; can't also be in label group."
+            )
 
     def validate_against(self, master_verbs):
         # Ensure that all the sets are disjoint, and that they exhaustively cover
@@ -44,13 +50,17 @@ class CombineLabels:
         union = set()
         total_len = 0
         for s in self.category_groups:
+            if isinstance(s, str):
+                s = {s}
             total_len += len(s)
             union |= s
         total_len += len(self.to_remove)
         union |= self.to_remove
-        assert len(union) == total_len
+        if len(union) != total_len:
+            raise ValueError("Overlapping sets!")
         set_master_verbs = set(master_verbs)
-        assert union == set_master_verbs, f"{sorted(union)} != {sorted(set_master_verbs)}"
+        if union != set_master_verbs:
+            raise ValueError(f"{sorted(union)} != {sorted(set_master_verbs)}")
 
     def get_final_idx(self, verb):
         return self.action_to_idx.get(verb)
