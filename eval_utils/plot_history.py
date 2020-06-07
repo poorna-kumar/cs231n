4961404105858fe28ae0c509c936caa085311ff6
Poorna Kumar <poorna.kumar@gmail.com>
2020-06-07 14:10:40 -0700

    change plot style

--- eval_utils/plot_history.py
+++ eval_utils/plot_history.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 
 def plot_history(history, *, y_lab):
-    plt.plot(history['train'], '-o', label='Train')
-    plt.plot(history['val'], '-o', label='Val')
+    plt.plot(history['train'], '-', label='Train')
+    plt.plot(history['val'], '-', label='Val')
     plt.xlabel("Epoch")
     plt.ylabel(y_lab)
     plt.legend()
